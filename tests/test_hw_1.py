7b5c0cc8a7413cb3744e819bca08809f4c5c037a
FarzanRashid <promit666@gmail.com>
2023-11-17 19:19:26 +0600

    Add Greeting test

--- tests/test_hw_1.py
+++ tests/test_hw_1.py
@@ -7,4 +7,22 @@ import hw
 class TestGreeting(TestCase):
     def test(self):
         g = hw.Greeting("hello", "world")
-        self.assertEqual(str(g),  "hello", "world")
+        self.assertEqual(str(g), "hello world")
+
+
+class TestMain(TestCase):
+    def setUp(self):
+        self.mock_greeting = Mock(
+            name='Greeting', return_value=Mock(
+                name="Greeting instance",
+                __str__=Mock(return_value="mock str output")))
+        self.mock_stdout = StringIO()
+
+    def test(self):
+        with patch("hw.Greeting", self.mock_greeting):
+            with patch("sys.stdout", self.mock_stdout):
+                hw.main()
+
+        self.mock_greeting.assert_called()
+        self.mock_greeting.return_value.__str__.assert_called_with()
+        self.assertEqual("mock str output\n", self.mock_stdout.getvalue())
