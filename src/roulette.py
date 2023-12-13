28ec583c8ef9e3d8b7328b4dd831e6be26fcc895
FarzanRashid <promit666@gmail.com>
2023-12-13 12:54:42 +0600

    Add winAmount and __eq__ methods for Outcome class

--- src/roulette.py
+++ src/roulette.py
@@ -2,3 +2,9 @@ class Outcome:
     def __init__(self, name: str, odds: int):
         self.name = name
         self.odds = odds
+
+    def winAmount(self, amount: float) -> float:
+        return self.odds * amount
+
+    def __eq__(self, other) -> bool:
+        return self.name == other.name
