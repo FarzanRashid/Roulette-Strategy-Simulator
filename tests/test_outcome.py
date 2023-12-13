a460bcaf1291ca0345483afdb6473d0a51d03901
FarzanRashid <promit666@gmail.com>
2023-12-13 19:34:53 +0600

    Implement equality test for Outcome objects

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -5,7 +5,9 @@ from unittest import TestCase
 
 class TestOutcome(TestCase):
     def setUp(self):
-        oc1 = Outcome("Red", 1)
-        oc2 = Outcome("Red", 1)
-        oc3 = Outcome("Black", 2)
+        self.oc1 = Outcome("Red", 1)
+        self.oc2 = Outcome("Red", 1)
+        self.oc3 = Outcome("Black", 2)
 
+    def test_equality(self):
+        self.assertEquals(self.oc1, self.oc2)
