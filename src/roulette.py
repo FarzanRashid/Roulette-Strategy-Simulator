c9b9fd847a23f1b53d65b52dc3a737efa7b2620c
FarzanRashid <promit666@gmail.com>
2023-12-13 18:34:19 +0600

    Add __str__ and __repr__ methods for Outcome class

--- src/roulette.py
+++ src/roulette.py
@@ -14,3 +14,9 @@ class Outcome:
 
     def __hash__(self) -> int:
         return hash(self.name)
+
+    def __str__(self) -> str:
+        return f"{self.name:s} ({self.odds:d}:1)"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__:s}(name={self.name!r}, odds={self.odds!r})"
