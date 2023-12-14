4d367751d95048ad8de3d7d7745da21e9eb23395
FarzanRashid <promit666@gmail.com>
2023-12-14 12:29:19 +0600

    Refactoring test method names

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -9,7 +9,7 @@ class TestOutcome(TestCase):
         self.oc2 = Outcome("Red", 1)
         self.oc3 = Outcome("Black", 2)
 
-    def test_equality(self):
+    def test_compare_objects_with_identical_names(self):
         self.assertEquals(self.oc1, self.oc2)
 
     def test_same_hash_values_for_equal_objects(self):
@@ -17,7 +17,7 @@ class TestOutcome(TestCase):
         oc2_hash_value = self.oc2.__hash__()
         self.assertEquals(oc1_hash_value, oc2_hash_value)
 
-    def test_winAmount_method(self):
+    def test_win_amount_calculation(self):
         oc3_winamount_value = self.oc3.winAmount(5)
         self.assertEquals(oc3_winamount_value, 10.0)
 
