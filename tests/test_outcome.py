c8f79e375a2fb68980362f8f4d3ca1652d69be93
FarzanRashid <promit666@gmail.com>
2023-12-17 14:11:00 +0600

    Address merge reviews

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -9,17 +9,19 @@ class TestOutcome(TestCase):
         self.oc2 = Outcome("Red", 1)
         self.oc3 = Outcome("Black", 2)
 
-    def test_compare_objects_with_identical_names(self):
-        self.assertEquals(self.oc1, self.oc2)
+    def test_objects_with_same_name_are_equal(self):
+        self.assertEqual(self.oc1, self.oc2)
 
     def test_same_hash_values_for_equal_objects(self):
-        oc1_hash_value = self.oc1.__hash__()
-        oc2_hash_value = self.oc2.__hash__()
-        self.assertEquals(oc1_hash_value, oc2_hash_value)
+        oc1_hash_value = hash(self.oc1)
+        oc2_hash_value = hash(self.oc2)
+        self.assertEqual(oc1_hash_value, oc2_hash_value)
 
     def test_win_amount_calculation(self):
-        oc3_winamount_value = self.oc3.winAmount(5)
-        self.assertEquals(oc3_winamount_value, 10.0)
+        amount = 5
+        win_amount = self.oc3.winAmount(amount)
+        expected_win_amount = 10.0
+        self.assertEqual(win_amount, expected_win_amount)
 
     def test_inequality_when_names_differ(self):
-        self.assertNotEquals(self.oc1, self.oc3)
+        self.assertNotEqual(self.oc1, self.oc3)
