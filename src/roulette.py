a332800d03d65dfeb4ee4f90744078d99443e456
FarzanRashid <promit666@gmail.com>
2023-12-26 16:23:53 +0600

    Fixed docstring formatting

--- src/roulette.py
+++ src/roulette.py
@@ -5,26 +5,27 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Outcome:
     """
-     :class:`Outcome` contains a single outcome on which a bet can be placed.
+    :class:`Outcome` contains a single outcome on which a bet can be placed.
 
-     In Roulette, each spin of the wheel has a number of :class:`Outcome` objects with bets that will be paid off. For
-     example, the “1” bin has the following winning :class:`Outcome` instances: “1”, “Red”, “Odd”, “Low”, “Column 1”,
-     “Dozen 1-12”, “Split 1-2”, “Split 1-4”, “Street 1-2-3”, “Corner 1-2-4-5”, “Five Bet”, “Line 1-2-3-4-5-6”,
-     “00-0-1-2-3”, “Dozen 1”, “Low” and “Column 1”.
+    In Roulette, each spin of the wheel has a number of :class:`Outcome` objects with bets that
+    will be paid off. For example, the “1” bin has the following winning :class:`Outcome`
+    instances: “1”, “Red”, “Odd”, “Low”, “Column 1”, “Dozen 1-12”, “Split 1-2”, “Split 1-4”,
+    “Street 1-2-3”, “Corner 1-2-4-5”, “Five Bet”, “Line 1-2-3-4-5-6”, “00-0-1-2-3”, “Dozen 1”,
+    “Low” and “Column 1”.
 
-     All of thee above-named bets will pay off if the wheel spins a “1”. This makes a Wheel and a Bin fairly
-     complex containers of :class:`Outcome` objects.
+    All of thee above-named bets will pay off if the wheel spins a “1”. This makes a Wheel and a
+    Bin fairly complex containers of :class:`Outcome` objects.
 
-     ..  attribute:: name
+    ..  attribute:: name
 
-         Holds the name of the Outcome. Examples include "1", "Red".
+        Holds the name of the Outcome. Examples include "1", "Red".
 
-     .. attribute:: odds
+    .. attribute:: odds
 
-         Holds the payout odds for this Outcome. Most odds are stated as 1:1 or 17:1, we only keep the numerator (17)
-         and assume the denominator is 1.
+        Holds the payout odds for this Outcome. Most odds are stated as 1:1 or 17:1, we only keep
+        the numerator (17) and assume the denominator is 1.
 
-     """
+    """
 
     name: str
     odds: int
@@ -50,40 +51,43 @@ class Outcome:
 
 class Bin(frozenset):
     """
-    :class:`Bin` contains a collection of :class:`Outcome` instances which reflect the winning bets that are paid for a
-    particular bin on a Roulette wheel. In Roulette, each spin of the wheel has a number of :class:`Outcome` instances.
-    Example: A spin of 1, selects the “1” bin with the following winning :class:`Outcome` instances: “1” , “Red”,
-    “Odd”,“Low” , “Column 1” , “Dozen 1-12” , “Split 1-2” , “Split 1-4” , “Street 1-2-3” , “Corner 1-2-4-5”, “Five Bet”,
-    “Line 1-2-3-4-5-6” , “00-0-1-2-3” , “Dozen 1”, “Low” and “Column 1”. These are collected into a single :class:`Bin`.
+    :class:`Bin` contains a collection of :class:`Outcome` instances which reflect the winning bets
+    that are paid for a particular bin on a Roulette wheel. In Roulette, each spin of the wheel
+    has a number of :class:`Outcome` instances. Example: A spin of 1, selects the “1” bin with
+    the following winning :class:`Outcome` instances: “1” , “Red”, “Odd”,“Low” , “Column 1” ,
+    “Dozen 1-12” , “Split 1-2” , “Split 1-4” , “Street 1-2-3” , “Corner 1-2-4-5”, “Five Bet”,
+    “Line 1-2-3-4-5-6” , “00-0-1-2-3” , “Dozen 1”, “Low” and “Column 1”. These are collected into a
+    single :class:`Bin`.
     """
-    pass
 
 
 class Wheel:
     """
-        :class:`Wheel` contains the 38 individual bins on a Roulette wheel, plus a random number generator. It can
-        select a :class:`Bin` at random, simulating a spin of the Roulette wheel.
+    :class:`Wheel` contains the 38 individual bins on a Roulette wheel, plus a random number
+    generator. It can select a :class:`Bin` at random, simulating a spin of the Roulette wheel.
 
-        .. attribute:: bins
+    .. attribute:: bins
 
-            Contains the individual Bin instances.
+        Contains the individual Bin instances.
 
-            This is a tuple of 38 elements. This can be built with ``tuple(Bin() for i in range(38))``
+        This is a tuple of 38 elements. This can be built with ``tuple(Bin() for i in
+        range(38))``
 
-        .. attribute:: rng
+    .. attribute:: rng
 
-            A random number generator to select a :class:`Bin` from the **bins** collection.
+        A random number generator to select a :class:`Bin` from the **bins** collection.
 
-            For testing, we’ll often want to seed this generator. For simulation processing, we can set the seed value
-            using ``os.urandom()``.
+        For testing, we’ll often want to seed this generator. For simulation processing, we can
+        set the seed value using ``os.urandom()``.
     """
 
     def __init__(self) -> None:
         """
-        Creates a new wheel with 38 empty Bin instances. It will also create a new random number generator instance.
+        Creates a new wheel with 38 empty Bin instances. It will also create a new random number
+        generator instance.
 
-        At the present time, this does not do the full initialization of the Bin instances. We’ll rework this in a
-        future exercise.
+        At the present time, this does not do the full initialization of the Bin instances. We’ll
+        rework this in a future exercise.
         """
         self.bins = tuple(Bin() for i in range(38))
         self.rng = random.Random()
@@ -104,8 +108,8 @@ class Wheel:
         """
         Generates a random number between 0 and 37, and returns the randomly selected Bin instance.
 
-        The **Random.choice()** function of the **random** module will select one of the available :class:`Bin` instances
-        from the **bins** collection.
+        The **Random.choice()** function of the **random** module will select one of the available
+        :class:`Bin` instances from the **bins** collection.
 
         :return: A Bin selected at random from the wheel.
         :rtype: Bin
