916651545b6264a351871be180bf4fe66f6cf838
FarzanRashid <promit666@gmail.com>
2024-01-04 19:34:36 +0600

    Add test to check if straight outcomes are generated properly

--- tests/test_bin_builder.py
+++ tests/test_bin_builder.py
@@ -6,3 +6,17 @@ class TestBinBuilder(TestCase):
     def setUp(self):
         self.bin_builder = BinBuilder()
         self.wheel = Wheel()
+
+    def test_if_straight_outcomes_are_generated_properly(self):
+        straight_bet_odds = 35
+        straight_outcomes = {
+            0: Outcome("0", straight_bet_odds),
+            1: Outcome("1", straight_bet_odds),
+            36: Outcome("36", straight_bet_odds),
+            37: Outcome("00", straight_bet_odds)
+            }
+
+        self.bin_builder.build_bins_for_straight_bets(self.wheel)
+
+        for bin_index in straight_outcomes:
+            self.assertIn(straight_outcomes[bin_index], self.wheel.bins[bin_index])
