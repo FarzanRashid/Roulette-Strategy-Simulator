a6c26f5bacf7ee6aa29e16801932c58df05ce0f4
FarzanRashid <promit666@gmail.com>
2024-01-04 19:07:17 +0600

    Add setUp method for testing

---
+++ tests/test_bin_builder.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from roulette import Outcome, Wheel, BinBuilder
+
+
+class TestBinBuilder(TestCase):
+    def setUp(self):
+        self.bin_builder = BinBuilder()
+        wheel = Wheel()
