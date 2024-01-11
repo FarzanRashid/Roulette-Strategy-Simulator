52ccedfd452573319cfc420466f34e0012e5b590
FarzanRashid <promit666@gmail.com>
2024-01-11 19:42:57 +0600

    Add test for Bet winAmount method

--- tests/test_bet.py
+++ tests/test_bet.py
@@ -8,3 +8,8 @@ class TestBet(TestCase):
         self.oc2 = Outcome("35", 35)
         self.bet1 = Bet(5, self.oc1)
         self.bet2 = Bet(10, self.oc2)
+
+    def test_winAmount_calculation(self):
+        win_amount = self.bet1.winAmount()
+        expected_win_amount = 10
+        self.assertEqual(expected_win_amount, win_amount)
