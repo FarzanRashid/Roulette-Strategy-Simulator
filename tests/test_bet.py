257f63eec11af567d116b1ee289a95f9cc6f65db
FarzanRashid <promit666@gmail.com>
2024-01-12 12:10:15 +0600

    Refactor test for winAmount method

--- tests/test_bet.py
+++ tests/test_bet.py
@@ -10,9 +10,12 @@ class TestBet(TestCase):
         self.bet2 = Bet(10, self.oc2)
 
     def test_winAmount_calculation(self):
-        win_amount = self.bet1.winAmount()
-        expected_win_amount = 10
-        self.assertEqual(expected_win_amount, win_amount)
+        bet_one_win_amount = self.bet1.winAmount()
+        bet_one_expected_win_amount = 10
+        bet_two_win_amount = self.bet2.winAmount()
+        bet_two_expected_win_amount = 360
+        self.assertEqual(bet_one_expected_win_amount, bet_one_win_amount)
+        self.assertEqual(bet_two_expected_win_amount, bet_two_win_amount)
 
     def test_loseAmount_calculation(self):
         bet_one_lose_amount = self.bet1.loseAmount()
