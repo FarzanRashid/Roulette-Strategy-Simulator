43be06576ab3f2c00f2785238f45fba1b7243f9e
FarzanRashid <promit666@gmail.com>
2024-01-14 00:47:53 +0600

    Refactor tests for binBuilder class

--- tests/test_bin_builder.py
+++ tests/test_bin_builder.py
@@ -19,8 +19,8 @@ class TestBinBuilder(TestCase):
 
         self.bin_builder.build_bins_for_straight_bets(self.wheel)
 
-        for bin_index in straight_outcomes:
-            self.assertIn(straight_outcomes[bin_index], self.wheel.bins[bin_index])
+        for bin_index, outcome in straight_outcomes.items():
+            self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_horizontal_split_bets(self):
         split_bet_odds = 17
@@ -33,10 +33,8 @@ class TestBinBuilder(TestCase):
 
         self.bin_builder.build_bins_for_horizontal_split_bets(self.wheel)
 
-        for bin_index in horizontal_split_outcomes:
-            self.assertIn(
-                horizontal_split_outcomes[bin_index], self.wheel.bins[bin_index]
-            )
+        for bin_index, outcome in horizontal_split_outcomes.items():
+            self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_vertical_split_bets(self):
         split_bet_odds = 17
@@ -49,10 +47,8 @@ class TestBinBuilder(TestCase):
 
         self.bin_builder.build_bins_for_vertical_split_bets(self.wheel)
 
-        for bin_index in vertical_split_outcomes:
-            self.assertIn(
-                vertical_split_outcomes[bin_index], self.wheel.bins[bin_index]
-            )
+        for bin_index, outcome in vertical_split_outcomes.items():
+            self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_street_bets(self):
         street_bet_odds = 11
@@ -63,8 +59,8 @@ class TestBinBuilder(TestCase):
 
         self.bin_builder.build_bins_for_street_bets(self.wheel)
 
-        for bin_index in street_outcomes:
-            self.assertIn(street_outcomes[bin_index], self.wheel.bins[bin_index])
+        for bin_index, outcome in street_outcomes.items():
+            self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_corner_bets(self):
         corner_bet_odds = 8
@@ -84,8 +80,11 @@ class TestBinBuilder(TestCase):
 
         self.bin_builder.build_bins_for_corner_bets(self.wheel)
 
-        for bin_index in corner_outcomes:
-            for outcome in corner_outcomes[bin_index]:
+        for (
+            bin_index,
+            outcomes,
+        ) in corner_outcomes.items():
+            for outcome in outcomes:
                 self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_line_bets(self):
@@ -100,37 +99,35 @@ class TestBinBuilder(TestCase):
 
         self.bin_builder.build_bins_for_line_bets(self.wheel)
 
-        for bin_index in line_outcomes:
-            for outcome in line_outcomes[bin_index]:
+        for bin_index, outcomes in line_outcomes.items():
+            for outcome in outcomes:
                 self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_dozen_bets(self):
         dozen_bet_odds = 2
         dozen_bet_outcomes = {
-            1: [Outcome("1-2-3-4-5-6-7-8-9-10-11-12", dozen_bet_odds)],
-            17: [Outcome("13-14-15-16-17-18-19-20-21-22-23-24", dozen_bet_odds)],
-            36: [Outcome("25-26-27-28-29-30-31-32-33-34-35-36", dozen_bet_odds)],
+            1: Outcome("1-2-3-4-5-6-7-8-9-10-11-12", dozen_bet_odds),
+            17: Outcome("13-14-15-16-17-18-19-20-21-22-23-24", dozen_bet_odds),
+            36: Outcome("25-26-27-28-29-30-31-32-33-34-35-36", dozen_bet_odds),
         }
 
         self.bin_builder.build_bins_for_dozen_bets(self.wheel)
 
-        for bin_index in dozen_bet_outcomes:
-            for outcome in dozen_bet_outcomes[bin_index]:
-                self.assertIn(outcome, self.wheel.bins[bin_index])
+        for bin_index, outcome in dozen_bet_outcomes.items():
+            self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filled_for_column_bets(self):
         column_bet_odds = 2
         column_bet_outcomes = {
-            1: [Outcome("-".join([str(_) for _ in range(1, 35, 3)]), column_bet_odds)],
-            17: [Outcome("-".join([str(_) for _ in range(2, 36, 3)]), column_bet_odds)],
-            36: [Outcome("-".join([str(_) for _ in range(3, 37, 3)]), column_bet_odds)],
+            1: Outcome("-".join([str(_) for _ in range(1, 35, 3)]), column_bet_odds),
+            17: Outcome("-".join([str(_) for _ in range(2, 36, 3)]), column_bet_odds),
+            36: Outcome("-".join([str(_) for _ in range(3, 37, 3)]), column_bet_odds),
         }
 
         self.bin_builder.build_bins_for_column_bets(self.wheel)
 
-        for bin_index in column_bet_outcomes:
-            for outcome in column_bet_outcomes[bin_index]:
-                self.assertIn(outcome, self.wheel.bins[bin_index])
+        for bin_index, outcome in column_bet_outcomes.items():
+            self.assertIn(outcome, self.wheel.bins[bin_index])
 
     def test_bins_are_filed_for_five_bet(self):
         five_bet_odds = 6
