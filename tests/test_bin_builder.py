33a9467db75c238ea8834053f96b224836bacc3c
FarzanRashid <promit666@gmail.com>
2024-01-13 18:02:31 +0600

    Refactor tests for binBuilder class

--- tests/test_bin_builder.py
+++ tests/test_bin_builder.py
@@ -14,8 +14,8 @@ class TestBinBuilder(TestCase):
             0: Outcome("0", straight_bet_odds),
             1: Outcome("1", straight_bet_odds),
             36: Outcome("36", straight_bet_odds),
-            37: Outcome("00", straight_bet_odds)
-            }
+            37: Outcome("00", straight_bet_odds),
+        }
 
         self.bin_builder.build_bins_for_straight_bets(self.wheel)
 
@@ -28,13 +28,15 @@ class TestBinBuilder(TestCase):
             1: Outcome("1-2", split_bet_odds),
             2: Outcome("1-2", split_bet_odds),
             35: Outcome("35-36", split_bet_odds),
-            36: Outcome("35-36", split_bet_odds)
+            36: Outcome("35-36", split_bet_odds),
         }
 
         self.bin_builder.build_bins_for_horizontal_split_bets(self.wheel)
 
         for bin_index in horizontal_split_outcomes:
-            self.assertIn(horizontal_split_outcomes[bin_index], self.wheel.bins[bin_index])
+            self.assertIn(
+                horizontal_split_outcomes[bin_index], self.wheel.bins[bin_index]
+            )
 
     def test_bins_are_filled_for_vertical_split_bets(self):
         split_bet_odds = 17
@@ -42,19 +44,21 @@ class TestBinBuilder(TestCase):
             1: Outcome("1-4", split_bet_odds),
             4: Outcome("1-4", split_bet_odds),
             33: Outcome("33-36", split_bet_odds),
-            36: Outcome("33-36", split_bet_odds)
+            36: Outcome("33-36", split_bet_odds),
         }
 
         self.bin_builder.build_bins_for_vertical_split_bets(self.wheel)
 
         for bin_index in vertical_split_outcomes:
-            self.assertIn(vertical_split_outcomes[bin_index], self.wheel.bins[bin_index])
+            self.assertIn(
+                vertical_split_outcomes[bin_index], self.wheel.bins[bin_index]
+            )
 
     def test_bins_are_filled_for_street_bets(self):
         street_bet_odds = 11
         street_outcomes = {
             1: Outcome("1-2-3", street_bet_odds),
-            36: Outcome("34-35-36", street_bet_odds)
+            36: Outcome("34-35-36", street_bet_odds),
         }
 
         self.bin_builder.build_bins_for_street_bets(self.wheel)
@@ -66,10 +70,17 @@ class TestBinBuilder(TestCase):
         corner_bet_odds = 8
         corner_outcomes = {
             1: [Outcome("1-2-4-5", corner_bet_odds)],
-            4: [Outcome("1-2-4-5", corner_bet_odds), Outcome("4-5-7-8", corner_bet_odds)],
-            5: [Outcome("1-2-4-5", corner_bet_odds), Outcome("4-5-7-8", corner_bet_odds),
-                Outcome("2-3-5-6", corner_bet_odds), Outcome("5-6-8-9", corner_bet_odds)]
-                }
+            4: [
+                Outcome("1-2-4-5", corner_bet_odds),
+                Outcome("4-5-7-8", corner_bet_odds),
+            ],
+            5: [
+                Outcome("1-2-4-5", corner_bet_odds),
+                Outcome("4-5-7-8", corner_bet_odds),
+                Outcome("2-3-5-6", corner_bet_odds),
+                Outcome("5-6-8-9", corner_bet_odds),
+            ],
+        }
 
         self.bin_builder.build_bins_for_corner_bets(self.wheel)
 
@@ -81,7 +92,10 @@ class TestBinBuilder(TestCase):
         line_bet_odds = 5
         line_outcomes = {
             1: [Outcome("1-2-3-4-5-6", line_bet_odds)],
-            4: [Outcome("1-2-3-4-5-6", line_bet_odds), Outcome("4-5-6-7-8-9", line_bet_odds)]
+            4: [
+                Outcome("1-2-3-4-5-6", line_bet_odds),
+                Outcome("4-5-6-7-8-9", line_bet_odds),
+            ],
         }
 
         self.bin_builder.build_bins_for_line_bets(self.wheel)
@@ -95,7 +109,7 @@ class TestBinBuilder(TestCase):
         dozen_bet_outcomes = {
             1: [Outcome("1-2-3-4-5-6-7-8-9-10-11-12", dozen_bet_odds)],
             17: [Outcome("13-14-15-16-17-18-19-20-21-22-23-24", dozen_bet_odds)],
-            36: [Outcome("25-26-27-28-29-30-31-32-33-34-35-36", dozen_bet_odds)]
+            36: [Outcome("25-26-27-28-29-30-31-32-33-34-35-36", dozen_bet_odds)],
         }
 
         self.bin_builder.build_bins_for_dozen_bets(self.wheel)
@@ -109,7 +123,7 @@ class TestBinBuilder(TestCase):
         column_bet_outcomes = {
             1: [Outcome("-".join([str(_) for _ in range(1, 35, 3)]), column_bet_odds)],
             17: [Outcome("-".join([str(_) for _ in range(2, 36, 3)]), column_bet_odds)],
-            36: [Outcome("-".join([str(_) for _ in range(3, 37, 3)]), column_bet_odds)]
+            36: [Outcome("-".join([str(_) for _ in range(3, 37, 3)]), column_bet_odds)],
         }
 
         self.bin_builder.build_bins_for_column_bets(self.wheel)
@@ -140,7 +154,26 @@ class TestBinBuilder(TestCase):
         even_bet_outcome = Outcome("Even", even_money_bet_odds)
         odd_bet_outcome = Outcome("Odd", even_money_bet_odds)
 
-        red_bet_numbers = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36}
+        red_bet_numbers = {
+            1,
+            3,
+            5,
+            7,
+            9,
+            12,
+            14,
+            16,
+            18,
+            19,
+            21,
+            23,
+            25,
+            27,
+            30,
+            32,
+            34,
+            36,
+        }
 
         even_bet_bin_indexes = {1, 17, 18, 36}
 
@@ -172,7 +205,6 @@ class TestBinBuilder(TestCase):
             "roulette.BinBuilder.build_bins_for_even_money_bets",
             "roulette.BinBuilder.build_bins_for_dozen_bets",
             "roulette.BinBuilder.build_bins_for_column_bets",
-
         }
 
         mock_helper_methods = []
