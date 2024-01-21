b9f15181c567830b19bb68d13de886dfd4392db9
FarzanRashid <promit666@gmail.com>
2024-01-21 13:07:52 +0600

    Add setUp method for testing

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,2 +1,11 @@
 from unittest import TestCase
 from roulette import Table, Bet, Outcome
+
+
+class TestTable(TestCase):
+    def setUp(self):
+        self.oc1 = Outcome("Black", 1)
+        self.oc2 = Outcome("1", 35)
+        self.bet1 = Bet(5, self.oc1)
+        self.bet2 = Bet(300, self.oc2)
+        self.table = Table(self.bet1, self.bet2)
