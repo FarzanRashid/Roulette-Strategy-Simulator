c895cda24bb8690f5ed9b300b97f27996c27a536
FarzanRashid <promit666@gmail.com>
2024-01-22 00:37:13 +0600

    Add test to check placeBet adds bet to the bet list

--- tests/test_table.py
+++ tests/test_table.py
@@ -9,3 +9,8 @@ class TestTable(TestCase):
         self.bet1 = Bet(5, self.oc1)
         self.bet2 = Bet(300, self.oc2)
         self.table = Table(self.bet1, self.bet2)
+
+    def test_placeBet_adds_bets_to_bets_list(self):
+        bet3 = Bet(30, self.oc1)
+        self.table.placeBet(bet3)
+        self.assertIn(bet3, self.table.bets)
