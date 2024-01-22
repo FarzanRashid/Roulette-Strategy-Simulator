e162b52d5ac544caec6ae23c0da4e7534da11abf
FarzanRashid <promit666@gmail.com>
2024-01-23 01:50:27 +0600

    Add docstring for InvalidBet class

--- src/roulette.py
+++ src/roulette.py
@@ -572,6 +572,12 @@ class Bet:
 
 
 class InvalidBet(Exception):
+    """
+    :class:`InvalidBet` is raised when a Player instance attempts to place a bet which exceeds the
+    table’s limit.
+
+    This class simply inherits all features of its superclass.
+    """
     pass
 
 
