09212ee93fd52716ff017f5a2da5352ed2064f82
FarzanRashid <promit666@gmail.com>
2024-01-26 19:34:53 +0600

    Add test to check cycle method calls placebet method

---
+++ tests/test_game.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from unittest.mock import Mock, patch
+
+from roulette import Wheel, Table, Passenger57, Game, BinBuilder
+
+
+class TestGame(TestCase):
+    def setUp(self):
+        self.wheel = Wheel()
+        self.table = Table()
+        bin_builder = BinBuilder()
+        bin_builder.buildBins(self.wheel)
+        self.game = Game(self.wheel, self.table)
+        self.passenger = Passenger57(self.table, self.wheel)
+
+    def test_cycle_calls_placeBets(self):
+        place_bets_mock = Mock(name="place_bets_mock")
+        with patch("roulette.Passenger57.placeBets", place_bets_mock):
+            self.game.cycle(self.passenger)
+        place_bets_mock.assert_called_once()
