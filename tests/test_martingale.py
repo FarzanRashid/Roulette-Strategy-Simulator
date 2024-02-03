ca8ffa307363d44fddad5596f2256024e4af395f
FarzanRashid <promit666@gmail.com>
2024-02-03 11:33:20 +0600

    Refactor test name for playing method

--- tests/test_martingale.py
+++ tests/test_martingale.py
@@ -44,7 +44,7 @@ class TestMartingale(TestCase):
         expected_stake = 110
         self.assertEqual(expected_stake, self.martingale.stake)
 
-    def test_playing_returns_false_if_stake_less_than_limit(self):
+    def test_player_plays_when_stake_not_less_than_table_minimum(self):
         self.martingale.stake = 3
         playing_result = self.martingale.playing()
         self.assertEqual(False, playing_result)
