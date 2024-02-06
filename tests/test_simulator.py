96f1859d9211aa7ea52b761ccfe06b21f2b96d3d
FarzanRashid <promit666@gmail.com>
2024-02-06 19:36:09 +0600

    Add test to check gather appends max stake and duration

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-
+from unittest.mock import Mock, patch
 from roulette import Simulator, Game, Martingale, Table, Wheel
 
 
@@ -11,3 +11,18 @@ class TestSimulator(TestCase):
         self.wheel = Wheel()
         self.game = Game(self.wheel, self.table)
         self.simulator = Simulator(self.game, self.martingale)
+
+    def test_gather_appends_max_stake_and_rounds_played_after_one_session(self):
+        session_mock = Mock(name="session_mock", return_value=[1, 2, 5])
+        self.simulator.samples = 1
+
+        with patch("roulette.Simulator.session", session_mock):
+            self.simulator.gather()
+
+        session_mock.assert_called_once()
+
+        expected_value_in_maxima = 5
+        self.assertIn(expected_value_in_maxima, self.simulator.maxima)
+
+        expected_length_in_duration = 3
+        self.assertIn(expected_length_in_duration, self.simulator.durations)
