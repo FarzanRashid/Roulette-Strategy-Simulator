4807593c9190d662d4a48e9aaf941f3c685672fc
FarzanRashid <promit666@gmail.com>
2024-02-13 21:28:27 +0600

    Fix failing test

--- tests/test_table.py
+++ tests/test_table.py
@@ -77,4 +77,4 @@ class TestTable(TestCase):
 
         expected_result_with_bets = f"Table({repr_string})"
 
-        self.assertNotEqual(expected_result_with_bets, repr_result_with_bets)
+        self.assertEqual(expected_result_with_bets, repr_result_with_bets)
