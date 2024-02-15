6933beefe1052b27e2892006360118a66da6908e
FarzanRashid <promit666@gmail.com>
2024-02-15 19:17:34 +0600

    Add placeBets method

--- src/players/seven_reds.py
+++ src/players/seven_reds.py
@@ -5,3 +5,7 @@ class SevenReds(Martingale):
     def __init__(self, table):
         super().__init__(table)
         self.redCount = 7
+
+    def placeBets(self) -> None:
+        if self.redCount == 0:
+            super().placeBets()
