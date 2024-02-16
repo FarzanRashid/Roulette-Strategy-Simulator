9bd55e002428bf27fd01f9d83d3011cb8f368b9e
FarzanRashid <promit666@gmail.com>
2024-02-16 18:48:36 +0600

    Fix documentation for SevenReds class

--- src/players/seven_reds.py
+++ src/players/seven_reds.py
@@ -8,6 +8,14 @@ class SevenReds(Martingale):
     :class:`SevenReds` is a :py:class:`~players.martingale.Martingale` player who places
     bets in Roulette. This player waits until the wheel has spun red seven times in a row before
     betting black.
+
+    .. attribute:: redCount
+
+       The number of reds yet to go. This starts at 7 , is reset to 7 on each non-red outcome, and
+       decrements by 1 on each red outcome.
+
+    **Note:** that this class inherits betMultiple. This is initially 1, doubles with each loss
+    and is reset to one on each win.
     """
 
     def __init__(self, table):
