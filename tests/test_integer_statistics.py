6bb5933142893e199651b4e816c92721d5aae7f6
FarzanRashid <promit666@gmail.com>
2024-02-19 00:28:13 +0600

    Add test to verify accurate mean calculation

--- tests/test_integer_statistics.py
+++ tests/test_integer_statistics.py
@@ -5,4 +5,10 @@ from integer_statistics import IntegerStatistics
 
 class TestIntegerStatistics(TestCase):
     def setUp(self):
-        int_stat = IntegerStatistics([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+        self.int_stat = IntegerStatistics([10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5])
+
+    def test_calculate_mean(self):
+        expected_mean_result = 9.0
+        actual_mean_result = self.int_stat.mean()
+
+        self.assertEqual(expected_mean_result, actual_mean_result)
