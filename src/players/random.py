37560a8bdfcf99eef12125515b50fa6972b018a8
FarzanRashid <promit666@gmail.com>
2024-02-20 20:20:34 +0600

    Implement PlayerRandom class

---
+++ src/players/random.py
@@ -0,0 +1,9 @@
+from players.player import Player
+
+
+class PlayerRandom(Player):
+    def __init__(self, table, wheel):
+        super().__init__(table)
+        self.wheel = wheel
+        bin_iterator = self.wheel.binIterator()
+        self.all_OC = set(outcome for bin in bin_iterator for outcome in bin)
