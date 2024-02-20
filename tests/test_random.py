df0b5125f26553364c90887c41c7b333318475d5
FarzanRashid <promit666@gmail.com>
2024-02-20 21:43:22 +0600

    Add test to verify all outcomes are added to all_OC

--- tests/test_random.py
+++ tests/test_random.py
@@ -1,11 +1,19 @@
 from unittest import TestCase
 from wheel import Wheel
 from table import Table
+from bin_builder import BinBuilder
 from players.random import PlayerRandom
 
 
 class TestRandom(TestCase):
     def setUp(self):
-        wheel = Wheel()
+        self.wheel = Wheel()
         table = Table()
-        self.random_player = PlayerRandom(table, wheel)
+        bin_builder = BinBuilder()
+        bin_builder.buildBins(self.wheel)
+        self.random_player = PlayerRandom(table, self.wheel)
+
+    def test_all_outcomes_are_added_to_all_OC(self):
+        all_outcomes = {outcome for outcome in self.wheel.all_outcomes.values()}
+
+        self.assertEqual(all_outcomes, self.random_player.all_OC)
