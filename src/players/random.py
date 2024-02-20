9e990b7f90f2515e876c49788e6cfbfb226b0f5a
FarzanRashid <promit666@gmail.com>
2024-02-20 20:23:30 +0600

    Fix __init__ method

--- src/players/random.py
+++ src/players/random.py
@@ -1,3 +1,5 @@
+import random
+
 from players.player import Player
 
 
@@ -5,5 +7,6 @@ class PlayerRandom(Player):
     def __init__(self, table, wheel):
         super().__init__(table)
         self.wheel = wheel
+        self.rng = random.Random()
         bin_iterator = self.wheel.binIterator()
         self.all_OC = set(outcome for bin in bin_iterator for outcome in bin)
