7cfc1e4df060a272e1fd92baf1b8cfd10d3d593c
FarzanRashid <promit666@gmail.com>
2024-02-21 10:47:49 +0600

    Add broken test for placeBets

--- tests/test_random.py
+++ tests/test_random.py
@@ -8,12 +8,20 @@ from players.random import PlayerRandom
 class TestRandom(TestCase):
     def setUp(self):
         self.wheel = Wheel()
-        table = Table()
+        self.table = Table()
         bin_builder = BinBuilder()
         bin_builder.buildBins(self.wheel)
-        self.random_player = PlayerRandom(table, self.wheel)
+        self.random_player = PlayerRandom(self.table, self.wheel)
 
     def test_all_outcomes_are_added_to_all_OC(self):
         all_outcomes = {outcome for outcome in self.wheel.all_outcomes.values()}
 
         self.assertEqual(all_outcomes, self.random_player.all_OC)
+
+    def test_placeBets_bets_on_randomly_selected_outcome_with_seed(self):
+        self.random_player.all_OC = list(self.random_player.all_OC)
+        fixed_seed = 1
+        self.random_player.rng.seed(fixed_seed)
+        radomly_selected_outcome = self.random_player.rng.choice(self.random_player.all_OC)
+        self.random_player.placeBets()
+        self.assertIn(radomly_selected_outcome, self.table.bets)
