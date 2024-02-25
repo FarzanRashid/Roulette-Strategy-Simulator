0abc9e5f9560a36440b74aa698239c8c54fded0c
FarzanRashid <promit666@gmail.com>
2024-02-26 00:10:40 +0600

    Implement Player1326OneWin class

--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -29,3 +29,8 @@ class Player1326NoWin(Player1326State):
 
     def nextWon(self) -> Player1326State:
         pass
+
+
+class Player1326OneWin(Player1326State):
+    def __init__(self, player: Player) -> None:
+        super().__init__(player)
