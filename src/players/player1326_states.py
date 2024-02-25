642a76b04a78e357a2778b048f9778b229cb62e5
FarzanRashid <promit666@gmail.com>
2024-02-26 00:11:23 +0600

    Add currentBet method

--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -34,3 +34,7 @@ class Player1326NoWin(Player1326State):
 class Player1326OneWin(Player1326State):
     def __init__(self, player: Player) -> None:
         super().__init__(player)
+
+    def currentBet(self) -> Bet:
+        bet_amount = 3
+        return Bet(bet_amount, self.player.outcome)
