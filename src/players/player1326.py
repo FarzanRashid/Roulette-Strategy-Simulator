60562dd0f0797d9a2fc0fa20145084f448632974
FarzanRashid <promit666@gmail.com>
2024-02-25 22:26:15 +0600

    Add currentBet method

--- src/players/player1326.py
+++ src/players/player1326.py
@@ -1,6 +1,12 @@
+from abc import abstractmethod
+from bet import Bet
 from player import Player
 
 
 class Player1326State:
     def __init__(self, player: Player) -> None:
         self.player = player
+
+    @abstractmethod
+    def currentBet(self) -> Bet:
+        return NotImplemented
