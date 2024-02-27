ace1d897c425764a94be6fc1d5041793d322371f
FarzanRashid <promit666@gmail.com>
2024-02-27 20:07:58 +0600

    Import dependencies for Player1326OneWin class

--- src/players/player1326/player1326_one_win.py
+++ src/players/player1326/player1326_one_win.py
@@ -1,7 +1,13 @@
+from bet import Bet
+from players.player1326.player1326 import Player1326
+from players.player1326.player1326_state import Player1326State
+from players.player1326.player1326_two_wins import Player1326TwoWins
+
+
 class Player1326OneWin(Player1326State):
     _player1326_onewin = None
 
-    def __init__(self, player: Player) -> None:
+    def __init__(self, player: Player1326) -> None:
         super().__init__(player)
         self.betAmount = 3
 
