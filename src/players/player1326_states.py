399731ee3ced472eeaa1e4bf6b3f5080c682bda0
FarzanRashid <promit666@gmail.com>
2024-02-27 19:42:57 +0600

    Move Player1326TwoWins class to a separate file

--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -3,25 +3,6 @@ from bet import Bet
 from players.player import Player
 
 
-class Player1326TwoWins(Player1326State):
-    _player1326_two_wins = None
-
-    def __init__(self, player: Player) -> None:
-        super().__init__(player)
-        self.betAmount = 2
-
-    def __new__(cls, *args, **kwargs) -> "Player1326TwoWins":
-        if cls._player1326_two_wins is None:
-            cls._player1326_two_wins = super().__new__(cls)
-        return cls._player1326_two_wins
-
-    def currentBet(self) -> Bet:
-        return Bet(self.betAmount, self.player.outcome)
-
-    def nextWon(self) -> Player1326State:
-        return Player1326ThreeWins(self.player)
-
-
 class Player1326ThreeWins(Player1326State):
     _player1326_three_wins = None
 
