47bb163e5d97c3e5b9ecfd69518404d8c0b5e4c6
FarzanRashid <promit666@gmail.com>
2024-02-27 12:21:37 +0600

    Add betAmount attributre to Player1326ThreeWins class

--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -80,6 +80,7 @@ class Player1326ThreeWins(Player1326State):
 
     def __init__(self, player: Player) -> None:
         super().__init__(player)
+        self.betAmount = 6
 
     def __new__(cls, *args, **kwargs) -> "Player1326ThreeWins":
         if cls._player1326_three_wins is None:
@@ -87,8 +88,7 @@ class Player1326ThreeWins(Player1326State):
         return cls._player1326_three_wins
 
     def currentBet(self) -> Bet:
-        bet_amount = 6
-        return Bet(bet_amount, self.player.outcome)
+        return Bet(self.betAmount, self.player.outcome)
 
     def nextWon(self) -> Player1326State:
         return Player1326NoWins(self.player)
