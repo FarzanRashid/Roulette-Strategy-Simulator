da6e4e7b568f09c87441989af5c915d952719997
FarzanRashid <promit666@gmail.com>
2024-02-27 12:45:12 +0600

    Add get method

--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -96,9 +96,12 @@ class Player1326ThreeWins(Player1326State):
 
 
 class Player1326StateFactory:
-    def __init__(self, player: Player):
+    def __init__(self, player: Player) -> None:
         self.values = {"Player1326NoWins": Player1326NoWins(player),
                        "Player1326OneWin": Player1326OneWin(player),
                        "Player1326TwoWins": Player1326TwoWins(player),
                        "Player1326ThreeWins": Player1326ThreeWins(player),
                        }
+
+    def get(self, name: str) -> Player1326State:
+        return self.values[name]
