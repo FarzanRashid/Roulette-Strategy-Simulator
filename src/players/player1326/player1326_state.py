d019bcb20356ce823d0c175aba472dcd0b51a7f4
FarzanRashid <promit666@gmail.com>
2024-02-27 19:46:58 +0600

    Move Player1326StateFactory class to Player1326_state file

--- src/players/player1326/player1326_state.py
+++ src/players/player1326/player1326_state.py
@@ -13,3 +13,15 @@ class Player1326State:
 
     def nextLost(self) -> "Player1326NoWins":
         return Player1326NoWins(self.player)
+
+
+class Player1326StateFactory:
+    def __init__(self, player: Player) -> None:
+        self.values = {"Player1326NoWins": Player1326NoWins(player),
+                       "Player1326OneWin": Player1326OneWin(player),
+                       "Player1326TwoWins": Player1326TwoWins(player),
+                       "Player1326ThreeWins": Player1326ThreeWins(player),
+                       }
+
+    def get(self, name: str) -> Player1326State:
+        return self.values[name]
--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -1,15 +1,3 @@
 from abc import abstractmethod
 from bet import Bet
 from players.player import Player
-
-
-class Player1326StateFactory:
-    def __init__(self, player: Player) -> None:
-        self.values = {"Player1326NoWins": Player1326NoWins(player),
-                       "Player1326OneWin": Player1326OneWin(player),
-                       "Player1326TwoWins": Player1326TwoWins(player),
-                       "Player1326ThreeWins": Player1326ThreeWins(player),
-                       }
-
-    def get(self, name: str) -> Player1326State:
-        return self.values[name]
