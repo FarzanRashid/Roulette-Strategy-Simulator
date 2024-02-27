a4d347b15bf6dad721d5850b6cd671d7e8ed4755
FarzanRashid <promit666@gmail.com>
2024-02-27 11:02:57 +0600

    Add __new__ method to Player1326TwoWins

--- src/players/player1326_states.py
+++ src/players/player1326_states.py
@@ -58,8 +58,14 @@ class Player1326OneWin(Player1326State):
 
 
 class Player1326TwoWins(Player1326State):
+    _player1326_two_wins = None
     def __init__(self, player: Player) -> None:
         super().__init__(player)
+
+    def __new__(cls, *args, **kwargs) -> "Player1326TwoWins":
+        if cls._player1326_two_wins is None:
+            cls._player1326_two_wins = super(Player1326TwoWins, cls).__new__(cls)
+        return cls._player1326_two_wins
 
     def currentBet(self) -> Bet:
         bet_amount = 2
