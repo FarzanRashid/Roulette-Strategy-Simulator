dfb7ed574c04e46a0cedb32ecdb1e368b6bbe1e6
FarzanRashid <promit666@gmail.com>
2024-03-01 20:23:04 +0600

    Add test to verify player1326 only plays when stake more than betAmount

--- tests/test_player1326/test_player1326.py
+++ tests/test_player1326/test_player1326.py
@@ -8,3 +8,12 @@ class TestPlayer1326(TestCase):
     def setUp(self):
         self.table = Table()
         self.player1326 = Player1326(self.table)
+
+    def test_player1326_plays_when_stake_higher_than_betAmount(self):
+        self.player1326.stake = 2
+        self.player1326.state.betAmount = 1
+        self.assertTrue(self.player1326.playing())
+
+        self.player1326.stake = 0
+
+        self.assertFalse(self.player1326.playing())
