aaa53c02addbb823e1d233ade2f73c2da7126938
FarzanRashid <promit666@gmail.com>
2024-03-01 21:40:49 +0600

    Add setUp method for testing

---
+++ tests/test_player1326/test_player1326_no_wins.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from players.player1326.player1326_no_wins import Player1326NoWins
+
+
+class TestPlayer1326NoWins(TestCase):
+    def setUp(self):
+        self.player1326_no_wins = Player1326NoWins
