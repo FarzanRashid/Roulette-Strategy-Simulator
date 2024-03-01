41600bc82bddb9fdbbfc19fedcd862f846893b77
FarzanRashid <promit666@gmail.com>
2024-03-01 21:20:07 +0600

    Add test to verify win changes state

--- tests/test_player1326/test_player1326.py
+++ tests/test_player1326/test_player1326.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 from table import Table
+from bet import Bet
+from outcome import Outcome
+from players.player1326.player1326_no_wins import Player1326NoWins
+from players.player1326.player1326_one_win import Player1326OneWin
 
 from players.player1326.player1326 import Player1326
 
@@ -35,3 +39,13 @@ class TestPlayer1326(TestCase):
         self.player1326.placeBets()
 
         self.assertEqual(expected_bet_after_bet, self.player1326.stake)
+
+    def test_win_changes_state_to_nextWon_state(self):
+        initial_state = Player1326NoWins()
+        self.assertEqual(initial_state, self.player1326.state)
+
+        self.player1326.win(Bet(2, Outcome("Red", 1)))
+
+        expected_state_after_win = Player1326OneWin()
+
+        self.assertEqual(self.player1326.state, expected_state_after_win)
