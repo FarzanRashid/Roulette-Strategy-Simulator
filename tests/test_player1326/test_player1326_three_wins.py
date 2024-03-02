9a604d4ad1afb78e6452dcb48d06a4ae0a5c7ff1
FarzanRashid <promit666@gmail.com>
2024-03-02 12:56:30 +0600

    Add test to verify only instance is created

--- tests/test_player1326/test_player1326_three_wins.py
+++ tests/test_player1326/test_player1326_three_wins.py
@@ -5,3 +5,10 @@ from players.player1326.player1326_state import Player1326ThreeWins
 class TestPlayer1326ThreeWins(TestCase):
     def setUp(self):
         self.player1326_three_wins = Player1326ThreeWins()
+
+    def test_only_one_instance_is_created(self):
+        instance_2 = Player1326ThreeWins()
+        self.assertIs(self.player1326_three_wins, instance_2)
+
+        instance_3 = Player1326ThreeWins()
+        self.assertIs(self.player1326_three_wins, instance_3)
