e0ce6b1f854ed8908326fb72a1321cd2a7f75c21
FarzanRashid <promit666@gmail.com>
2024-03-02 13:11:20 +0600

    Add test to check nextLost changes state

--- tests/test_player1326/test_player1326_state.py
+++ tests/test_player1326/test_player1326_state.py
@@ -1,7 +1,13 @@
 from unittest import TestCase
-from players.player1326.player1326_state import Player1326State
+from players.player1326.player1326_state import Player1326State, Player1326NoWins
 
 
 class TestPlayer1326State(TestCase):
     def setUp(self):
         self.player1326_state = Player1326State()
+
+    def test_nextLost_changes_state(self):
+        no_win_state = Player1326NoWins()
+        nextLost_result = self.player1326_state.nextLost()
+
+        self.assertIs(no_win_state, nextLost_result)
