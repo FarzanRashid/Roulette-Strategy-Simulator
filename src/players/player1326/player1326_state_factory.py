4715409bc42e537e77161b0b0ab3aede2ec28095
FarzanRashid <promit666@gmail.com>
2024-03-02 11:11:45 +0600

    Fix import path

--- src/players/player1326/player1326_state_factory.py
+++ src/players/player1326/player1326_state_factory.py
@@ -1,8 +1,5 @@
-from players.player1326.player1326_no_wins import Player1326NoWins
-from players.player1326.player1326_one_win import Player1326OneWin
-from players.player1326.player1326_state import Player1326State
-from players.player1326.player1326_two_wins import Player1326TwoWins
-from players.player1326.player1326_three_wins import Player1326ThreeWins
+from players.player1326.player1326_state import Player1326State, Player1326NoWins, Player1326OneWin
+from players.player1326.player1326_state import Player1326TwoWins, Player1326ThreeWins
 
 
 class Player1326StateFactory:
