b6b1c9f4674dc9c2eebe22bdbf1d9a2d47d16e90
FarzanRashid <promit666@gmail.com>
2024-03-02 10:53:13 +0600

    Move Player1326TwoWins to avoid circular import

--- src/players/player1326/player1326_state.py
+++ src/players/player1326/player1326_state.py
@@ -57,3 +57,22 @@ class Player1326OneWin(Player1326State):
 
     def nextWon(self) -> Player1326State:
         return Player1326TwoWins()
+
+
+class Player1326TwoWins(Player1326State):
+    _player1326_two_wins = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.betAmount = 2
+
+    def __new__(cls, *args, **kwargs) -> "Player1326TwoWins":
+        if cls._player1326_two_wins is None:
+            cls._player1326_two_wins = super().__new__(cls)
+        return cls._player1326_two_wins
+
+    def currentBet(self) -> Bet:
+        return Bet(self.betAmount, self.outcome)
+
+    def nextWon(self) -> Player1326State:
+        return Player1326ThreeWins()
