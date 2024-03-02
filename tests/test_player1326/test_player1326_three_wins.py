1497c3ddbd5426822e58869d98ebbd6ceb3ca89a
FarzanRashid <promit666@gmail.com>
2024-03-02 12:54:40 +0600

    Add setUp method for testing

---
+++ tests/test_player1326/test_player1326_three_wins.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+from players.player1326.player1326_state import Player1326ThreeWins
+
+
+class TestPlayer1326ThreeWins(TestCase):
+    def setUp(self):
+        self.player1326_three_wins = Player1326ThreeWins()
