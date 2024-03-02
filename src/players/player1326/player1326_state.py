44b0ad7d715b57f6eb22b6fa218f380bf2f75f8c
FarzanRashid <promit666@gmail.com>
2024-03-02 10:54:32 +0600

    Move Player1326ThreeWins to avoid circular import

--- src/players/player1326/player1326_state.py
+++ src/players/player1326/player1326_state.py
@@ -76,3 +76,22 @@ class Player1326TwoWins(Player1326State):
 
     def nextWon(self) -> Player1326State:
         return Player1326ThreeWins()
+
+
+class Player1326ThreeWins(Player1326State):
+    _player1326_three_wins = None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.betAmount = 6
+
+    def __new__(cls, *args, **kwargs) -> "Player1326ThreeWins":
+        if cls._player1326_three_wins is None:
+            cls._player1326_three_wins = super().__new__(cls)
+        return cls._player1326_three_wins
+
+    def currentBet(self) -> Bet:
+        return Bet(self.betAmount, self.outcome)
+
+    def nextWon(self) -> Player1326State:
+        return super().nextLost()
--- src/players/player1326/player1326_three_wins.py
+++ src/players/player1326/player1326_three_wins.py
@@ -2,20 +2,3 @@ from bet import Bet
 from players.player1326.player1326_state import Player1326State
 
 
-class Player1326ThreeWins(Player1326State):
-    _player1326_three_wins = None
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.betAmount = 6
-
-    def __new__(cls, *args, **kwargs) -> "Player1326ThreeWins":
-        if cls._player1326_three_wins is None:
-            cls._player1326_three_wins = super().__new__(cls)
-        return cls._player1326_three_wins
-
-    def currentBet(self) -> Bet:
-        return Bet(self.betAmount, self.outcome)
-
-    def nextWon(self) -> Player1326State:
-        return super().nextLost()
