178d6a7af9a3d445c2d897add0bbda983f897353
FarzanRashid <promit666@gmail.com>
2024-03-02 12:11:39 +0600

    Add setuP method for testing

---
+++ tests/test_player1326/test_player1326_two_wins.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from players.player1326.player1326_state import Player1326TwoWins
+
+
+class TestPlayer1326TwoWins(TestCase):
+    def setUp(self):
+        self.player1326_two_wins = Player1326TwoWins()
