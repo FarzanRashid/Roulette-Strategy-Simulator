f3be8958acd9fa37d44e1c878bab9cc567cc5120
FarzanRashid <promit666@gmail.com>
2024-03-02 11:45:48 +0600

    Add setUp method for testing

---
+++ tests/test_player1326/test_player1326_one_win.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from players.player1326.player1326_state import Player1326OneWin
+
+
+class TestPlayer1326OneWin(TestCase):
+    def setUp(self):
+        self.player1326_one_win = Player1326OneWin()
+
