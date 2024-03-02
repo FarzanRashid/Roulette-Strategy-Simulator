a2ed00c983e4c601d694d314d6c2ee72440afa2d
FarzanRashid <promit666@gmail.com>
2024-03-02 13:00:17 +0600

    Add test to verify currentBet returns Bet

--- tests/test_player1326/test_player1326_three_wins.py
+++ tests/test_player1326/test_player1326_three_wins.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+from bet import Bet
 from players.player1326.player1326_state import Player1326ThreeWins
 
 
@@ -12,3 +13,9 @@ class TestPlayer1326ThreeWins(TestCase):
 
         instance_3 = Player1326ThreeWins()
         self.assertIs(self.player1326_three_wins, instance_3)
+
+    def test_currentBet_returns_bet(self):
+        expected_bet = Bet(self.player1326_three_wins.betAmount, self.player1326_three_wins.outcome)
+        actual_bet = self.player1326_three_wins.currentBet()
+
+        self.assertEqual(expected_bet, actual_bet)
