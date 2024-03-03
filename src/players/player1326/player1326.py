d0a40ac83757fd44aede9810e9b7e3979a4b9599
FarzanRashid <promit666@gmail.com>
2024-03-03 20:12:05 +0600

    Add docstring for Player1326 class

--- src/players/player1326/player1326.py
+++ src/players/player1326/player1326.py
@@ -5,6 +5,13 @@ from players.player1326.player1326_state_factory import Player1326StateFactory
 
 
 class Player1326(Player):
+    """
+    :class:`Player1326` follows the 1-3-2-6 betting system. The player has a preferred
+    :py:class:`~outcome.Outcome` instance. This should be an even money bet like red, black,
+    even, odd, high or low. The player also has a current betting state that determines the current
+    bet to place, and what next state applies when the bet has won or lost.
+    """
+
     def __init__(self, table: Table) -> None:
         super().__init__(table)
         self.table = table
