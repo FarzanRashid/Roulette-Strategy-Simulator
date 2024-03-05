c7595d3cb3803c62d5a2d0aaf7dc9321f5555650
FarzanRashid <promit666@gmail.com>
2024-03-05 16:26:24 +0600

    Add test to verify resetSequence resets sequence

--- tests/test_cancellation_player.py
+++ tests/test_cancellation_player.py
@@ -7,3 +7,9 @@ class TestPlayerCancellation(TestCase):
     def setUp(self):
         self.table = Table()
         self.player_cancellation = PlayerCancellation(self.table)
+
+    def test_resetSequence_resets_sequence(self):
+        self.player_cancellation.sequence = []
+        expected_seq_after_resetSequence = [1, 2, 3, 4, 5, 6]
+        self.player_cancellation.resetSequence()
+        self.assertEqual(self.player_cancellation.sequence, expected_seq_after_resetSequence)
