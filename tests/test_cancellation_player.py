70bd5bf9371d55f05a95386a8efcc81e92003901
FarzanRashid <promit666@gmail.com>
2024-03-05 16:35:09 +0600

    Add test to check lose adds bet amount to sequence

--- tests/test_cancellation_player.py
+++ tests/test_cancellation_player.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from table import Table
+from bet import Bet
+from outcome import Outcome
 from players.cancellation_player import PlayerCancellation
 
 
@@ -13,3 +15,10 @@ class TestPlayerCancellation(TestCase):
         expected_seq_after_resetSequence = [1, 2, 3, 4, 5, 6]
         self.player_cancellation.resetSequence()
         self.assertEqual(self.player_cancellation.sequence, expected_seq_after_resetSequence)
+
+    def test_lose_adds_bet_amount_to_sequence(self):
+        bet = Bet(10, Outcome("Red", 1))
+        self.assertNotIn(bet.amount, self.player_cancellation.sequence)
+
+        self.player_cancellation.lose(bet)
+        self.assertIn(bet.amount, self.player_cancellation.sequence)
