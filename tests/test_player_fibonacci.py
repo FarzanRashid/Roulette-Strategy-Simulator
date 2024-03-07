c7377ada545f2842c1691e0a2764733759c83b86
FarzanRashid <promit666@gmail.com>
2024-03-07 20:36:15 +0600

    Add test to check win resets recent value

--- tests/test_player_fibonacci.py
+++ tests/test_player_fibonacci.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from table import Table
+from bet import Bet
 from players.player_fibonacci import PlayerFibonacci
 
 
@@ -7,3 +8,12 @@ class TestPlayerFibonacci(TestCase):
     def setUp(self):
         self.table = Table()
         self.player_fibonacci = PlayerFibonacci(self.table)
+        self.bet = Bet(1, self.player_fibonacci.outcome)
+
+    def test_win_resets_recent_value(self):
+        self.player_fibonacci.recent = 10
+        expected_recent_value_after_win = 1
+
+        self.player_fibonacci.win(self.bet)
+
+        self.assertEqual(self.player_fibonacci.recent, expected_recent_value_after_win)
