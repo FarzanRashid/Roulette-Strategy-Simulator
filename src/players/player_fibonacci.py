78e7a9dd6409d2d004cda100d2682a4376c3e905
FarzanRashid <promit666@gmail.com>
2024-03-07 20:09:39 +0600

    Add win method

--- src/players/player_fibonacci.py
+++ src/players/player_fibonacci.py
@@ -1,3 +1,4 @@
+from bet import Bet
 from players.player import Player
 from table import Table
 from outcome import Outcome
@@ -7,5 +8,10 @@ class PlayerFibonacci(Player):
     def __init__(self, table: Table) -> None:
         super().__init__(table)
         self.outcome = Outcome("Black", 1)
+        self.recent = 1
+        self.previous = 0
+
+    def win(self, bet: Bet) -> None:
+        super().win(bet)
         self.recent = 1
         self.previous = 0
