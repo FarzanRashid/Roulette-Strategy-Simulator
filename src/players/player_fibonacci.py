0997fe33baab2f62731729e4d16d4a4dd67bfbec
FarzanRashid <promit666@gmail.com>
2024-03-07 20:19:35 +0600

    Add lose method

--- src/players/player_fibonacci.py
+++ src/players/player_fibonacci.py
@@ -10,8 +10,14 @@ class PlayerFibonacci(Player):
         self.outcome = Outcome("Black", 1)
         self.recent = 1
         self.previous = 0
+        self.bet_amount = self.recent + self.previous
 
     def win(self, bet: Bet) -> None:
         super().win(bet)
         self.recent = 1
         self.previous = 0
+
+    def lose(self, bet: Bet) -> None:
+        self.bet_amount = self.recent + self.previous
+        self.previous = self.recent
+        self.recent = self.bet_amount
