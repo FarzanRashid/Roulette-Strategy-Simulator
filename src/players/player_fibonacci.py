4cd45be54644adda1fd4e1907c2ec3bdb95036d6
FarzanRashid <promit666@gmail.com>
2024-03-07 21:22:57 +0600

    Removes duplicate playing condition check

--- src/players/player_fibonacci.py
+++ src/players/player_fibonacci.py
@@ -23,7 +23,7 @@ class PlayerFibonacci(Player):
         self.recent = self.bet_amount
 
     def playing(self) -> bool:
-        return super().playing() and self.roundsToGo > 0 and self.stake >= self.bet_amount
+        return super().playing() and self.stake >= self.bet_amount
 
     def placeBets(self) -> None:
         self.table.placeBet(Bet(self.bet_amount, self.outcome))
