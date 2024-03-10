6fbcb290f25d8f8fce9f6b16b44cf37da771cadc
FarzanRashid <promit666@gmail.com>
2024-03-11 01:31:55 +0600

    Refactor test

--- tests/test_cancellation.py
+++ tests/test_cancellation.py
@@ -77,9 +77,9 @@ class TestPlayerCancellation(TestCase):
 
     def test_sequence_is_reset_when_player_is_not_playing(self):
         self.player_cancellation.sequence = []
-        self.player_cancellation.stake = 0
         expected_sequence_after_playing = [1, 2, 3, 4, 5, 6]
 
+        self.assertFalse(self.player_cancellation.playing())
         self.player_cancellation.playing()
         self.assertEqual(
             expected_sequence_after_playing, self.player_cancellation.sequence
