28e99739a85705a3f46661a80aa027cb962c8425
FarzanRashid <promit666@gmail.com>
2024-04-04 02:05:38 +0600

    Add test to verify provide_player raises Valueerror

--- tests/test_strategy_factory.py
+++ tests/test_strategy_factory.py
@@ -12,19 +12,25 @@ class TestPlayerProvide(TestCase):
     def setUp(self):
         self.table = Table()
         self.wheel = Wheel()
-
-    def test_provide_player_provides_player(self):
         bin_builder = BinBuilder()
         bin_builder.buildBins(self.wheel)
 
+    def test_provide_player_provides_player(self):
         martingale_player = provide_player("Martingale", self.table, self.wheel)
-
         self.assertIsInstance(martingale_player, Martingale)
 
         sevenreds_player = provide_player("Sevenreds", self.table, self.wheel)
-
         self.assertIsInstance(sevenreds_player, SevenReds)
 
         fibonacci_player = provide_player("Fibonacci", self.table, self.wheel)
-
         self.assertIsInstance(fibonacci_player, PlayerFibonacci)
+
+    def test_provide_player_raise_error_when_player_name_not_valid(self):
+        with self.assertRaises(ValueError):
+            provide_player("Martingale123", self.table, self.wheel)
+
+        with self.assertRaises(ValueError):
+            provide_player("SevenReds01", self.table, self.wheel)
+
+        with self.assertRaises(ValueError):
+            provide_player("FIBONACCI789", self.table, self.wheel)
