b355cd871edd083573b7f3ffa74b1c03a6cb6881
FarzanRashid <promit666@gmail.com>
2024-04-04 00:16:16 +0600

    Refactor main method

--- src/roulette.py
+++ src/roulette.py
@@ -8,7 +8,7 @@ from strategy_factory import provide_strategy
 
 
 @click.command()
-@click.option('--strategy_name', prompt='Enter a strategy')
+@click.option("--strategy_name", prompt="Enter a strategy")
 def main(strategy_name) -> None:  # pragma: no cover
     """
     A main application function that creates the necessary objects, runs the Simulator’s gather()
@@ -19,13 +19,14 @@ def main(strategy_name) -> None:  # pragma: no cover
     table = Table()
     game = Game(wheel, table)
     bin_builder.buildBins(wheel)
-    player = provide_strategy(strategy_name, table, wheel)
+    player = provide_strategy(strategy_name.capitalize(), table, wheel)
     simulator = Simulator(game, player)
     simulator.gather()
 
+    print(f"Simulating {strategy_name} strategy \n")
     print("maxima: ", simulator.maxima)
     print("Mean of maxima:", simulator.maxima.mean())
-    print("Standard deviation of maxima:", simulator.maxima.stdev())
+    print("Standard deviation of maxima:", simulator.maxima.stdev(), "\n")
 
     print("duration: ", simulator.durations)
     print("Mean of duration:", simulator.durations.mean())
