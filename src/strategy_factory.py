0c5682c27a43bebc9ad9bdfc5da7f02d049b940d
FarzanRashid <promit666@gmail.com>
2024-04-03 22:08:34 +0600

    Add method

--- src/strategy_factory.py
+++ src/strategy_factory.py
@@ -8,3 +8,18 @@ from players.seven_reds import SevenReds
 from players.random import PlayerRandom
 from players.passenger57 import Passenger57
 from players.player1326.player1326 import Player1326
+
+
+def provide_strategy(strategy_name: str, table: Table, wheel: Wheel) -> Player:
+    strategies = {
+        "Martingale": Martingale(table),
+        "Cancellation": PlayerCancellation(table),
+        "Fibonacci": PlayerFibonacci(table),
+        "Sevenreds": SevenReds(table),
+        "Random": PlayerRandom(table, wheel),
+        "Passenger57": Passenger57(table, wheel),
+        "Player1326": Player1326(table),
+    }
+    if strategy_name not in strategies:
+        raise KeyError("Strategy not found, enter a valid strategy name")
+    return strategies[strategy_name]
