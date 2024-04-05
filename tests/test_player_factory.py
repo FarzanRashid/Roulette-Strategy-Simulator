e61d40f631ce31a996b38fec6cf2089b0acc6a3b
FarzanRashid <promit666@gmail.com>
2024-04-05 13:08:30 +0600

    Add players for testing

--- tests/test_player_factory.py
+++ tests/test_player_factory.py
@@ -6,6 +6,8 @@ from player_factory import player_factory
 from players.martingale import Martingale
 from players.fibonacci import PlayerFibonacci
 from players.seven_reds import SevenReds
+from players.random import PlayerRandom
+from players.passenger57 import Passenger57
 
 
 class TestPlayerFactory(TestCase):
@@ -24,6 +26,12 @@ class TestPlayerFactory(TestCase):
 
         fibonacci_player = player_factory("Fibonacci", self.table, self.wheel)
         self.assertIsInstance(fibonacci_player, PlayerFibonacci)
+
+        random_player = player_factory("Random", self.table, self.wheel)
+        self.assertIsInstance(random_player, PlayerRandom)
+
+        passenger57_player = player_factory("Passenger57", self.table, self.wheel)
+        self.assertIsInstance(passenger57_player, Passenger57)
 
     def test_player_factory_raise_error_when_player_name_not_valid(self):
         with self.assertRaises(ValueError):
